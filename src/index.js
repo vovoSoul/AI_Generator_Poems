74d8f87712c24dc8f9bd1de8d677f11e4ddf8928
vovoSoul <164639325+vovoSoul@users.noreply.github.com>
2024-04-24 13:30:18 -0400

    added js to the submit button
    edited some html structure

--- src/index.js
+++ src/index.js
@@ -1,1 +1,15 @@
+function changePoem(event) {
+  event.preventDefault();
 
+  new Typewriter("#newpoem", {
+    strings: `Poetry is the spontaneous overflow of powerful feelings: it takes its
+        origin from emotion recollected in tranquility."
+        <div>- William Wordsworth</div>`,
+    autoStart: true,
+    cursor: "",
+    delay: 20,
+  });
+}
+
+let searchForm = document.querySelector("#search");
+searchForm.addEventListener("submit", changePoem);
